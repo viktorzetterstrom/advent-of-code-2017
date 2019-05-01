2f0982c672daf0490fe523eb89991bcdf7ac1777
Viktor Zetterström <viktorzetterstrom@gmail.com>
2019-05-01 19:52:10 +0200

    solve day 4

--- 4/high-entropy-passphrases.js
+++ 4/high-entropy-passphrases.js
@@ -1,20 +1,36 @@
 // Problem: https://adventofcode.com/2017/day/4
-
 const fs = require('fs');
+
 const input = fs
   .readFileSync('./input.txt')
   .toString()
   .split('\n')
   .map(row => row.split(' '));
 
-const containsNoDuplicates = (row) => {
-  return row.length === new Set(row).size;
+const containsNoDuplicates = passphrase =>
+  passphrase.length === new Set(passphrase).size;
+
+const containsNoAnagram = (passphrase) => {
+  let sortedWords = [];
+  for (let word of passphrase)
+    sortedWords.push(
+      word.split('').sort().join('')
+    );
+  return containsNoDuplicates(sortedWords);
 };
 
-const validPhrases = input.reduce((count, row) => {
-  return containsNoDuplicates(row)
+
+const validPhrases1 = input.reduce((count, passphrase) => {
+  return containsNoDuplicates(passphrase)
     ? count += 1
     : count;
 }, 0);
 
-  console.log('Part 1:', input);
+const validPhrases2 = input.reduce((count, passphrase) => {
+  return containsNoAnagram(passphrase)
+    ? count += 1
+    : count;
+}, 0);
+
+console.log('Part 1:', validPhrases1);
+console.log('Part 2:', validPhrases2);
